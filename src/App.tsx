5fbd6fa8fdff57a7c9200a56333bf88508badcd9
FuzzyFade <1339184537@qq.com>
2022-03-20 18:54:14 +0800

    feat: 美化 header 栏

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Loading from "./components/loading";
 import utf16to8 from "./lib/util";
-import { useAppReady, useSafeArea } from "mincu-react";
+import { eventModule, useAppReady } from "mincu-react";
 import QRCode from "react-qr-code";
-import { dataModule, networkModule } from "mincu-react";
+import { dataModule, uiModule } from "mincu-react";
 import * as ReactGA from "react-ga";
 import "./App.css";
 
@@ -11,10 +11,10 @@ ReactGA.initialize("UA-80324378-26");
 
 const App = () => {
   const isReady = useAppReady();
-  const { bottom } = useSafeArea();
+  const [show, setShow] = useState(true);
 
   useEffect(() => {
-    networkModule.getStoredToken();
+    uiModule.handleShowHeader(false);
     ReactGA.pageview(location.pathname);
   }, []);
 
@@ -30,11 +30,49 @@ const App = () => {
     return <Loading />;
   }
 
+  const renderHeader = () => {
+    return (
+      <div className="header" style={{ paddingTop: dataModule.inset.top }}>
+        <img
+          src={"/exit.svg"}
+          className="exit"
+          onClick={() => uiModule.exit()}
+        />
+        <div className="title">南大核酸码</div>
+
+        <div style={{ flex: 1 }} />
+        <img
+          src={"/more.svg"}
+          className="more"
+          onClick={() => eventModule.showShare()}
+        />
+      </div>
+    );
+  };
+
+  const renderTips = () => {
+    if (!show) return null;
+
+    return (
+      <>
+        <div style={{ height: "40px" }}></div>
+        <div className="tip">
+          本码若无法正常使用，可点击复制下面的链接到【微信】
+        </div>
+        <div className="tip-2">https://jhrz.ncu.edu.cn/ndhsjc/student/#/</div>
+
+        <div className="tip-x" style={{ bottom: dataModule.inset.bottom + 20 }}>
+          为战胜新冠疫情，保证南大核酸码被大规模推广使用，本微应用作为南大核酸码快捷入口供同学们使用。
+        </div>
+      </>
+    );
+  };
+
   return (
-    <div>
+    <div onClick={() => setShow(!show)}>
+      {renderHeader()}
       <div
         style={{
-          // marginTop: `${top + 36}px`,
           marginTop: `36px`,
           marginRight: 10,
           marginLeft: 10,
@@ -49,15 +87,7 @@ const App = () => {
         <div style={{ height: "36px" }}></div>
         <QRCode value={JSON.stringify(qrData)} fgColor={"#1D3A74"} size={320} />
 
-        <div style={{ height: "40px" }}></div>
-        <div className="tip">
-          本码若无法正常使用，可点击复制下面的链接到【微信】
-        </div>
-        <div className="tip-2">https://jhrz.ncu.edu.cn/ndhsjc/student/#/</div>
-
-        <div className="tip-x" style={{ bottom: bottom + 20 }}>
-          为战胜新冠疫情，保证南大核酸码被大规模推广使用，本微应用作为南大核酸码快捷入口供同学们使用。
-        </div>
+        {renderTips()}
       </div>
     </div>
   );
