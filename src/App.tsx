a37c68e955292cabf39cb8a32c8692e87e8b9d31
FuzzyFade <1339184537@qq.com>
2022-03-20 15:55:20 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+import Loading from "./components/loading";
+import { useLogin } from "./hooks/useLogin";
+import { useSafeArea } from "./hooks/useSafeArea";
+import QRCode from "react-qr-code";
+import {
+  dataModule,
+  networkModule,
+} from "mincu-react";
+import './App.css';
+
+const App = () => {
+  const { isReady } = useLogin();
+  const { top } = useSafeArea();
+
+  useEffect(() => {
+    networkModule.getStoredToken();
+  }, []);
+
+  if (!isReady) {
+    return <Loading />;
+  }
+
+  const xh = dataModule.userInfo.profile.entireProfile?.base_info?.xh ?? "";
+  const xm = dataModule.userInfo.profile.entireProfile?.base_info?.xm ?? "";
+
+  const qrData = {
+    userId: xh,
+    name: xm,
+  };
+
+  return (
+    <div>
+      <div
+        style={{
+          marginTop: '36px',
+          marginRight: 10,
+          marginLeft: 10,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <div className="text">请在核酸排队时出示</div>
+        <div className="text">姓名: {xm}</div>
+        <div className="text">学号/工号/B类ID: {xh}</div>
+        <div style={{ height: "36px" }}></div>
+        <QRCode value={JSON.stringify(qrData)} fgColor={'#1D3A74'} size={320}/>
+      </div>
+    </div>
+  );
+};
+
+export default App;
